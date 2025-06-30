eca9e4db700f9b5957097618e057b985253777bb
unknown <nirmaladeshmukh2297@gmail.com>
2025-06-30 11:36:23 +0530

    added comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { Pipe1Pipe } from './shared/pipe1.pipe';
+import { Dir1Directive } from './shared/dir1.directive';
+import { MaxProtectComponent } from './health/max-protect/max-protect.component';
+import { PersonalProtectComponent } from './health/personal-protect/personal-protect.component';
+import { CarInsComponent } from './car-ins/car-ins.component';
+import { CarIncComponent } from './motor/car-inc/car-inc.component';
+import { MotorIncComponent } from './motor/motor-inc/motor-inc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Pipe1Pipe,
+    Dir1Directive,
+    MaxProtectComponent,
+    PersonalProtectComponent,
+    CarInsComponent,
+    CarIncComponent,
+    MotorIncComponent
   ],
   imports: [
     BrowserModule
